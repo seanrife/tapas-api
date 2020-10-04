c84ddfec1bde0e3a06aefe60453ab469acfeef99
seanrife <seanrife@gmail.com>
2020-10-04 10:58:19 -0500

    Modified app

--- app.py
+++ app.py
@@ -4,29 +4,15 @@ from flask import Flask, render_template, request
 from coolname import generate_slug
 from zipfile import ZipFile
 from db import get_cursor
-import socket
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 UPLOAD_FOLDER = '/home/sean/tapas/uploads'
 
 
-def send_file(filename):
-    s = socket.socket()
-    host = '54.186.172.17'
-    port = 10069
-    s.connect((host, port))
-    with open(filename, 'rb') as f:
-        data = f.read(1024)
-        while (data):
-            s.send(data)
-            data = f.read(1024)
-        s.close
-
-
 @app.route('/')
 def table_upload():
-    return render_template('index.html', file_list=['File 1', 'File 2'])
+    return render_template('index.html', file_list=['File'])
 
 
 @app.route('/upload', methods=['POST'])
@@ -59,8 +45,6 @@ def upload():
     with ZipFile(arcfilename, 'w') as newzip:
         for file in os.listdir(os.path.join(UPLOAD_FOLDER, slug)):
             newzip.write(os.path.join(UPLOAD_FOLDER, slug, file), arcname=file)
-
-    send_file(arcfilename)
 
     link = 'https://tapas.ai/{}'.format(slug)
     return render_template('upload_complete.html', link=link)
